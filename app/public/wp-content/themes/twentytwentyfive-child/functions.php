2a10581322742f95cda870e12c619cb9601a6159
Lakshdeep Singh <lakshdeep28singh@outlook.com>
2025-10-29 00:53:56 -0400

    Removed unused themes and activated child theme

---
+++ app/public/wp-content/themes/twentytwentyfive-child/functions.php
@@ -0,0 +1,5 @@
+<?php
+function greengoods_enqueue_styles() {
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+}
+add_action('wp_enqueue_scripts', 'greengoods_enqueue_styles');
